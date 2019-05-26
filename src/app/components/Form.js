4e462515f6e6a13d894fc6d65b874e2eb51357e2
vc7 <vince78718@gmail.com>
2019-05-26 15:58:48 +0900

    Able to trigger ctrl+enter.

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-export const Form = (props) => {
-    return (
-        <div className="container">
-            <form className="form-inline">
-                <input type="text" className="form-control"/>
-                <button className="btn btn-primary ml-3">Submit</button>
-            </form>
-        </div>
-    );
+export class Form extends React.Component {
+
+    onSubmit(event) {
+        event.preventDefault();
+    }
+
+    onKeyDown(event) {
+        if ((event.ctrlKey || event.metaKey) && (event.keyCode === 13 || event.keyCode === 10)) {
+            console.log("cmd+enter detected");
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <form className="form-inline" onSubmit={(event) => this.onSubmit(event)}>
+                    <input type="text" className="form-control" onKeyDown={(event) => this.onKeyDown(event)}/>
+                    <button className="btn btn-primary ml-3">Submit</button>
+                </form>
+            </div>
+        );
+    }
 };
